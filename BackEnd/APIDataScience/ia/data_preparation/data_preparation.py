1f06f67bb5a5f2b130619a4b455ac9a9fb56839e
Carlos Mejia <camh670@gmail.com>
2023-11-16 18:00:31 -0500

    Nueva función: 	-> Backend-MPD-ia: se agrega función para preparar los datos para las prediciones

--- BackEnd/APIDataScience/ia/data_preparation/data_preparation.py
+++ BackEnd/APIDataScience/ia/data_preparation/data_preparation.py
@@ -1,10 +1,15 @@
 import pandas as pd
+import pickle
+import os
+from datetime import datetime, timedelta
+
+from prophet import Prophet
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from common.exceptions.custom_exceptions import send_exception
 
 
-def data_preparation(df_fecha, df_producto, df_demanda):
+def preparate_train_data(df_fecha, df_producto, df_demanda):
     """
     Prepares data to be sent to the ia model for training or validation
     
@@ -24,11 +29,38 @@ def data_preparation(df_fecha, df_producto, df_demanda):
     # Renombra las columnas para cumplir con los requisitos de Prophet
     df.rename(columns={'fecha': 'ds', 'cantidad_real': 'y', 'producto_id': 'producto'}, inplace=True)
 
-
-
     # Selecciona solo las columnas necesarias para el modelo
     df_modelo = df[['ds', 'y', 'producto']]
 
     return df_modelo
+
+
+def preparate_inference_data(start_date_str, df_productos, num_periods):
+    """
+        Prepare data for Prophet model forecasts for multiple products and dates.
+
+        Args:
+        start_date_str (str): The start date for predictions in 'YYYYY-MM-DD' format.
+        df_productos (DataFrame): DataFrame with the list of products.
+        num_periods (int): Number of days to generate dates from the start date.
+
+        Returns:
+            DataFrame: A DataFrame ready to use in Prophet predictions.
+        """
+    # Convert start_date from str to datetime
+    start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+
+    # Generate a range of future dates
+    fechas_futuras = [start_date + timedelta(days=x) for x in range(num_periods)]
+    df_fechas_futuras = pd.DataFrame(fechas_futuras, columns=['ds'])
+
+    # Create a DataFrame with all date and product combinations
+    df_prediccion = pd.merge(df_fechas_futuras.assign(key=1), df_productos.assign(key=1), on='key').drop('key', axis=1)
+
+    # Sort and select the required columnsO
+    df_prediccion = df_prediccion[['ds', 'producto_id']]
+
+    return df_prediccion
+
             
 
--- BackEnd/APIDataScience/main.py
+++ BackEnd/APIDataScience/main.py
@@ -9,7 +9,7 @@ from config.properties import getProperty
 from DB.connection import connect
 import pandas as pd
 
-from ia.data_preparation.data_preparation import data_preparation
+from ia.data_preparation.data_preparation import preparate_train_data, preparate_inference_data
 from ia.model_training.training_model import train_models
 
 name_DB_ETLS = getProperty('DATABASEETLS')
@@ -24,8 +24,10 @@ df_fecha = pd.read_sql(f'SELECT * FROM DimFecha', con=ses_db_data_science)
 df_producto = pd.read_sql(f'SELECT * FROM DimProducto', con=ses_db_data_science)
 df_demanda = pd.read_sql(f'SELECT * FROM HechosDemandaProducto', con=ses_db_data_science)
 
-result=data_preparation(df_fecha,df_producto,df_demanda)
+result_data_train=preparate_train_data(df_fecha, df_producto, df_demanda)
 
-print(result.head(10))
-
-train_models(result)
+print(result_data_train.tail(10))
+result_data_inference=preparate_inference_data('2023-11-16',df_producto,30)
+print(result_data_inference.head(10))
+print(result_data_inference.tail(10))
+#train_models(result)
